9fc17d16a6d8af28cf577a154a54e2145d53550b
Koning <30795560+Koningcs@users.noreply.github.com>
2020-10-29 22:15:22 +0800

    Update BlackHole.cpp

--- SimpleFPSTemplate/Source/FPSGame/Private/Challenges/BlackHole.cpp
+++ SimpleFPSTemplate/Source/FPSGame/Private/Challenges/BlackHole.cpp
@@ -12,29 +12,59 @@ ABlackHole::ABlackHole()
 	PrimaryActorTick.bCanEverTick = true;
 
 	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
-	RootComponent = MeshComp;
 	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-
-	SphereAttactComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereAttactComp"));
-	SphereAttactComp->SetupAttachment(MeshComp);
+	RootComponent = MeshComp;
 
 	SphereDesComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereDesComp"));
 	SphereDesComp->SetupAttachment(MeshComp);
+	//SphereDesComp->SetCollisionResponseToAllChannels(ECR_Overlap);
+	SphereDesComp->InitSphereRadius(100.0f);
+
+	SphereDesComp->OnComponentBeginOverlap.AddDynamic(this, &ABlackHole::OverlapSphereDesComp);
+
+
+	SphereAttactComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereAttactComp"));
+	SphereAttactComp->SetupAttachment(MeshComp);
+	SphereAttactComp->InitSphereRadius(3000.0f);
+
+
+
+
+	//SphereDesComp->
+	//SphereDesComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 
 
 }
 
+void ABlackHole::OverlapSphereDesComp(UPrimitiveComponent* OverlapSphereDesComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Your message"));
+	if (OtherActor) OtherActor->Destroy();
+}
+
+
 // Called when the game starts or when spawned
 void ABlackHole::BeginPlay()
 {
+
 	Super::BeginPlay();
-	GetOwner()->
+	//GetOwner()->
 }
 
 // Called every frame
 void ABlackHole::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	TArray<UPrimitiveComponent*> SomeComponent;
+	SphereAttactComp->GetOverlappingComponents(SomeComponent);
+	for (int32 i = 0; i < SomeComponent.Num(); i++) {
+		if (SomeComponent[i] && SomeComponent[i]->IsSimulatingPhysics()) {
+			const float SphereRadius = SphereAttactComp->GetScaledSphereRadius();
+			const float ForceStrength = -2000;
+			SomeComponent[i]->AddRadialForce(GetActorLocation(), SphereRadius, ForceStrength, ERadialImpulseFalloff::RIF_Constant, true);
+		}
+	}
 }
-
+	
+	
